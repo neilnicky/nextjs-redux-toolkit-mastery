88fd64e891549c64ef539ac2190d2398ed2bc013
neilnicky <neilnicholaskanat@gmail.com>
2025-01-06 13:15:39 +0530

    feat: use selectors to display state on frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { RootState } from "@/store";
+import { login, logout } from "@/store/authSlice";
 import { decrement, increment, reset } from "@/store/counterSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function Home() {
   const count = useSelector((state: RootState) => state.counter.value);
+  const user = useSelector((state: RootState) => state.auth.user);
   const dispatch = useDispatch();
 
   return (
@@ -17,6 +19,13 @@ export default function Home() {
           <button onClick={() => dispatch(decrement())}>-</button>
         </div>
         <button onClick={() => dispatch(reset())}>Reset</button>
+      </div>
+      <div>
+        <div className="gap-4 flex text-center">
+          <button onClick={() => dispatch(login("Neil Nicky"))}>Login</button>
+          <button onClick={() => dispatch(logout())}>Logout</button>
+        </div>
+        {user && <h1>Logged in as {user}</h1>}
       </div>
     </div>
   );
