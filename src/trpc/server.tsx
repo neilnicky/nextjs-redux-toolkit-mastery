1623abf0b518929351b4ca7773f9a6aaa8ebc0cb
neilnicky <neilnicholaskanat@gmail.com>
2025-01-11 18:58:18 +0530

    refactor: rename and refactor AuthRouter for consistency

--- src/trpc/server.tsx
+++ src/trpc/server.tsx
@@ -3,7 +3,7 @@ import { createHydrationHelpers } from '@trpc/react-query/rsc';
 import { cache } from 'react';
 import { createCallerFactory, createTRPCContext } from './init';
 import { makeQueryClient } from './query-client';
-import { appRouter } from './routers/_app';
+import { appRouter } from './routers/auth';
 // IMPORTANT: Create a stable getter for the query client that
 //            will return the same client during the same request.
 export const getQueryClient = cache(makeQueryClient);
